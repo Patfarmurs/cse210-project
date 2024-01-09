49ee447d6bd27fc89d5d6275322fed83418ee351
unknown <patrickkeita80@yahoo.com>
2024-01-09 07:44:03 -0800

    Finish prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter student's score: ");
+        string userInput = Console.ReadLine();
+        int score = int.Parse(userInput);
+
+        char grade;
+
+        if (score>= 90)
+        {
+            grade = 'A';
+        }
+
+        else if (score >= 80)
+        {
+            grade = 'B';
+        }
+
+         else if (score >= 70)
+        {
+            grade = 'C';
+        }
+
+         else if (score >= 60)
+        {
+            grade = 'D';
+        }
+
+        else
+        {
+            grade = 'F';
+        }
+
+        Console.WriteLine($"The student's grade is: {grade}");
+
+        if (score >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Good luck next time!");
+        } 
     }
 }
