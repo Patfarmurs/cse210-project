b551ac46338fa7cdc9d0c3ec3779c2610ba0e190
unknown <patrickkeita80@yahoo.com>
2024-01-13 06:53:05 -0800

    Finish prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int input;
+        do
+        {
+            Console.Write("Enter a number (enter 0 to quit): ");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        } while (input != 0);
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = numbers.Average();
+        Console.WriteLine($"The average is: {average}");
+
+        int maxNumber = numbers.Max();
+        Console.WriteLine($"The maximum number is: {maxNumber}");
     }
 }
