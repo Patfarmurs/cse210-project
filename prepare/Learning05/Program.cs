2f2342b719d24ad5e2ae95a6155a9800c15916c4
unknown <patrickkeita80@yahoo.com>
2024-02-11 02:16:29 -0800

    Finish learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 5.0);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Green", 4.0, 6.0);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Blue", 3.0);
+        shapes.Add(circle);
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.Color}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
+
+        // Testing a single Square instance
+        Square singleSquare = new Square("Yellow", 4.0);
+        Console.WriteLine($"Color: {singleSquare.Color}");
+        Console.WriteLine($"Area: {singleSquare.GetArea()}");
     }
 }
